2ac42b32bbd7a3080b734094e84efc62ed8be50b
therealharshhacker <37790426+therealharshhacker@users.noreply.github.com>
2021-05-25 08:51:06 +0530

    Update RDP_Coffin.ps1

--- RDP_Coffin.ps1
+++ RDP_Coffin.ps1
@@ -1,10 +1,10 @@
-Set-LocalUser -Name "Administrator" -Password (ConvertTo-SecureString -AsPlainText "$PASSWORD" -Force)
+Set-LocalUser -Name "Administrator" -Password (ConvertTo-SecureString -AsPlainText "@#coffin123456" -Force)
 Get-LocalUser -Name "Administrator" | Enable-LocalUser 
 Invoke-WebRequest https://raw.githubusercontent.com/c9ffin/rdpw/main/ngrok-stable-windows-amd64.zip -OutFile ngrok.zip
 tar xf ngrok.zip
 Copy ngrok.exe C:\Windows\System32
 Start-Service -Name audiosrv
-cmd /c echo ./ngrok.exe authtoken "$NGROK" >a.ps1
+cmd /c echo ./ngrok.exe authtoken "1t0jEMsB9IUnoMBUsLQuxd041Zn_4E3vJo3bhUCm8C7Et59Nr" >a.ps1
 cmd /c echo cmd /k start ngrok.exe tcp 3389 >>a.ps1
 cmd /c echo ping -n 999999 10.10.10.10 >>a.ps1
  Write-Host "Visit: https://dashboard.ngrok.com/endpoints/status to get NGROK IP"
